eaff2b0ddb21e156b48004d8482c3c5131b73c5a
Jonathan <jon_nowak13@yahoo.com>
2018-12-09 04:09:50 -0800

    Started implementing client side data validation

--- index.php
+++ index.php
@@ -3,34 +3,17 @@
 require 'library.php';
 
 if (isset($_POST['login'])) {
-	if (strlen($_POST["login_username"]) < 8 || strlen($_POST["login_password"]) < 8) {
-		echo "Your username and password must consist of at least 8 characters";
-	} else if (!ctype_alnum($_POST["login_username"]) || !ctype_alnum($_POST["login_password"])) {
-		echo "Your username and password must consist of only alphanumeric characters";
-	} else if ($user_id = Login($_POST["login_username"], $_POST["login_password"])) {
-		session_start();
-		$_SESSION['user_id'] = $user_id;
-		header("Location: dashboard.php");
-	} else {
-		echo "Invalid username or password";
-	}
+	$user_id = Login($_POST["login_username"], $_POST["login_password"]);
+	session_start();
+	$_SESSION['user_id'] = $user_id;
+	header("Location: dashboard.php");
 }
 
 if (isset($_POST['signup'])) {
-	if (strlen($_POST["signup_username"]) < 8 || strlen($_POST["signup_password_1"]) < 8 || strlen($_POST["signup_password_2"]) < 8) {
-		echo "Your username and password must consist of at least 8 characters";
-	} else if (!ctype_alnum($_POST["signup_username"]) || !ctype_alnum($_POST["signup_password_1"]) || !ctype_alnum($_POST["signup_password_2"])) {
-		echo "Your username and password must consist of only alphanumeric characters";
-	} else if (UsernameExists($_POST["signup_username"])) {
-		echo "That username is taken";
-	} else if ($_POST["signup_password_1"] != $_POST["signup_password_2"]) {
-		echo "Those passwords do not match";
-	} else {
-		$user_id = Signup($_POST["signup_username"], $_POST["signup_password_1"]);
-		session_start();
-		$_SESSION['user_id'] = $user_id;
-		header("Location: dashboard.php");
-	}
+	$user_id = Signup($_POST["signup_username"], $_POST["signup_password_1"]);
+	session_start();
+	$_SESSION['user_id'] = $user_id;
+	header("Location: dashboard.php");
 }
 
 ?>
@@ -42,6 +25,7 @@ if (isset($_POST['signup'])) {
 		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 		<link rel="stylesheet" href="styling.css">
 		<link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
@@ -101,7 +85,30 @@ $().ready(function() {
 		$("#login_password_response").html($("#login_password").val());
     });
 	$("#signup_username").keyup(function() {
-		$("#signup_username_response").html($("#signup_username").val());
+		if ($("#signup_username").val() != "") {
+			if ($("#signup_username").val().length < 8) {
+				$("#signup_username_response").show();
+				$("#signup_username_response").html("Your username must be at least 8 characters long");
+			} else {
+				var dataString = "username=" + $("#signup_username").val();
+				$.ajax({
+					type: "POST",
+					url: "username_check.php",
+					data: dataString,
+					cache: false,
+					success: function(result) {
+						if (result == "available") {
+							$("#signup_username_response").hide();
+						} else {
+							$("#signup_username_response").show();
+							$("#signup_username_response").html(result);
+						}
+					}
+				});
+			}
+		} else {
+			$("#signup_username_response").hide();
+		}
     });
 	$("#signup_password_1").keyup(function() {
 		$("#signup_password_1_response").html($("#signup_password_1").val());
--- username_check.php
+++ username_check.php
@@ -1,10 +1,14 @@
 <?php
 
 $database = new PDO("mysql:host=localhost;dbname=db", "root", "");
-$statement = $database -> prepare("SELECT COUNT(*) FROM users WHERE username = :username");
+$statement = $database -> prepare("SELECT user_id FROM users WHERE username = :username");
 $statement -> bindParam(':username', $_POST['username']);
 $statement -> execute();
 $result = $statement -> fetchAll();
-echo sizeof($result);
+if (sizeof($result) == 1) {
+	echo "That username is taken";
+} else {
+	echo "available";
+}
 
 ?>
